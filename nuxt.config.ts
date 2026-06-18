f717c91fa43e88021c181f89d8bc8a8a4cf8f9dd
diewhy <korotkov671@gmail.com>
2026-06-19 00:15:18 +0300

    yandex map5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
   runtimeConfig: {
-    openaiApiKey: process.env.OPENAI_API_KEY
+    openaiApiKey: process.env.OPENAI_API_KEY,
+
+    public: {
+      yandexMapsKey: process.env.NUXT_PUBLIC_YANDEX_MAPS_KEY
+    }
   }
 })
